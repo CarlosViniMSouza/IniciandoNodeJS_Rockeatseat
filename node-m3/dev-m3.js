0d6a879312dd5f47f3df49574f2df558371a9d2b
Carlos Vinicius <60679101+CarlosViniMSouza@users.noreply.github.com>
2020-04-18 13:25:56 -0400

    dev-m3.js com menos codigo

--- node-m3/dev-m3.js
+++ node-m3/dev-m3.js
@@ -1,9 +1,7 @@
 const http = require('http')
 const fs = require('fs')
 const path = require('path')
-
 http.createServer((req, res) => {
-
   if (req.url === '/') {
       fs.readFile(
         path.join(__dirname, 'public', 'index.html'),
