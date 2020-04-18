fc91576f04e6e681e96963698fce6e60d421e24e
Carlos Vinicius <60679101+CarlosViniMSouza@users.noreply.github.com>
2020-04-18 10:25:23 -0400

    Concluido node-m1

--- node-m1/intro.js
+++ node-m1/intro.js
@@ -1,1 +1,11 @@
 // Iniciando MasterClass na Rockeatseat!
+
+// Momento inicial: 00:00 ~ 16:40 - Mapa mental
+// Motor V-8 do Chrome e Spider-Monkey(Firefox)
+
+// Vejamos alguns modulos do Node: 16:50
+
+/* 
+- www.nodejs.org => Para mais informações
+- cd youtube-nodejsclassmaster-node => repositório GitHub do professor.
+*/ 
